36963817d7505a6ba260931dc0f30c49b0127173
Aaron Chan <aarchan108@gmail.com>
2025-08-19 17:40:05 -0400

    Save chroma to numpy template

--- src/chorus_bin_gen.py
+++ src/chorus_bin_gen.py
@@ -15,9 +15,16 @@ def main():
         print("Input wav file does not exist")
         sys.exit(1)
 
+    hop_length = 512
     audio_time_series, sampling_rate = librosa.load(input_wav)
+    chroma = librosa.feature.chroma_cqt(y=audio_time_series, sr=sampling_rate, hop_length=hop_length)
+    chroma = chroma / (np.linalg.norm(chroma, axis=0, keepdims=True) + 1e-9)
 
+    print(audio_time_series.shape)
+    print("Sampling rate: ", sampling_rate)
 
+    np.save(output_npy, chroma)
+    print("Saved numpy template to " + output_npy)
 
 if __name__ == '__main__':
     main()
