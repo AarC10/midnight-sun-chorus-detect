1e73f4948f7469021383fa5bfb2ff770ab22eb1a
Aaron Chan <aarchan108@gmail.com>
2025-08-20 03:03:56 -0400

    Configure stuff we probably want

---
+++ src/detect_chorus.py
@@ -0,0 +1,13 @@
+# Config
+SR = 22050 # derived from generator script
+HOP = 512
+BLOCK_SECONDS = 3.0        # analysis window size
+STEP_SECONDS  = 0.75       # slide step
+THRESHOLD = 0.78           # DTW score/length (lower==better)
+REQUIRED_HITS = 2          # consecutive windows below threshold
+SERIAL_PORT = "/dev/ttyUSB0"
+SERIAL_BAUD = 921600
+SERIAL_LINE = b"brightness 7000\n"
+DEBOUNCE_SECONDS = 7.0     # don't re-trigger faster than this
+TEMPLATE_PATH = "chorus_template.npy"
+INPUT_DEVICE = None        # set to device index or None for default
